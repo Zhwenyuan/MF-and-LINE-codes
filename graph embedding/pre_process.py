5f9e626e4303a6cb72349c31e56d1b36730aa1d2
Zhwenyuan <42182507+Zhwenyuan@users.noreply.github.com>
2018-12-18 10:21:57 +0800

    Update pre_process.py

--- graph embedding/pre_process.py
+++ graph embedding/pre_process.py
@@ -1,25 +1,26 @@
-import re
 import numpy as np
-import torch
-from torch import nn, functional
-from torch.autograd import Variable
 
-f = open('beidian_shopkeepers_and_fans_filted_1_2_zyx.txt','r')
-fdata = f.read()
-re.sub('\s','\t',fdata)
-spliteddata = fdata.split('\n')
-spliteddata.pop() #最后一个是个空格
-shopkeepers = []
-users = []
-for d in spliteddata:
-    couple = d.split()
-    if not couple[0] in shopkeepers:
-        shopkeepers.append(couple[0])
-    if not couple[1] in users:
-        users.append(couple[1])
-relationmap = np.zeros((len(shopkeepers),len(users)))
-for i in range(len(shopkeepers)):
-    for j in range(len(users)):
-        couple = shopkeepers[i]+'\t'+users[j]
-        if couple in spliteddata:
-            relationmap[i][j] = 1
+
+def getMatrixFromFile():
+    f = open('.\\beidian_shopkeepers_and_fans_filted_1_2_zyx.txt', 'r')
+    fans = set()
+    shopkeeperes = set()
+    for line in f:
+        strlist = line.split('\t')
+        fans.add(strlist[1])
+        shopkeeperes.add((strlist[0]))
+
+    fans = list(fans)
+    shopkeeperes = list(shopkeeperes)
+    relationmap = np.zeros([fans.__len__(), shopkeeperes.__len__()], dtype=np.uint8)
+
+    count = 0
+    for line in f:
+        count += 1
+        if(count%1000==0):
+            print("loop:{}".format(count))
+        strlist = line.split('\t')
+        if strlist[0] in shopkeeperes and strlist[1] in fans:
+            relationmap[fans.index(strlist[1]), shopkeeperes.index(strlist[0])] = 1
+
+    return relationmap, fans, shopkeeperes
