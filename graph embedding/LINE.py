6a96b81a8ef48d78e4a24b4da269d839e9632ec0
Zhwenyuan <42182507+Zhwenyuan@users.noreply.github.com>
2018-12-18 10:21:23 +0800

    Update LINE.py

--- graph embedding/LINE.py
+++ graph embedding/LINE.py
@@ -2,6 +2,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
+import datetime
+import pre_process
 
 
 class LINE(nn.Module):
@@ -15,28 +17,31 @@ class LINE(nn.Module):
         return sigmoid_func(self.fan_factors(torch.tensor(range(n_fans))).mm(self.shopkeeper_factors(torch.tensor(range(n_shopkeepers))).t()))
 
 
-# class MyLoss(nn.Module):
-#     def __init__(self):
-#         super(MyLoss, self).__init__()
-#
-#     def forward(self, P, weight=1):
-#         return -(weight * torch.log(P)).sum()
+def train():
 
+    curtime = datetime.datetime.now()
+    print("%d: %d: %d\n" % (curtime.hour, curtime.minute, curtime.second))
+    print("initializing relationmap...")
 
-if __name__ == '__main__':
-    arr = np.array([[0, 0, 0, 1, 0],
-                    [1, 0, 0, 0, 1],
-                    [0, 1, 0, 0, 0],
-                    [1, 0, 0, 1, 0]])
-    rows, cols = arr.shape
+    relationmap, arr1, arr2 = pre_process.getMatrixFromFile()
+    print("relationmap initializing completed.\n")
 
-    model = LINE(4, 5, n_factors=10)
-    weights = torch.tensor(arr, dtype=torch.float)
+    print("utilizing graph embedding...\nlearning rate = 1e-3, latent factors = 10\n")
+    # select nonzero rows and cols
+    rows, cols = relationmap.nonzero()
+    p = np.random.permutation(len(rows))
+    rows, cols = rows[p[:10000]], cols[p[:10000]]
+
+    n_fans, n_shopkeepers = relationmap.shape
+    model = LINE(n_fans, n_shopkeepers, n_factors=10)
+    weights = torch.sparse.FloatTensor(torch.tensor(rows), torch.tensor(cols))
     loss_func = nn.BCELoss(weight=weights, reduce=True, size_average=False)
-    optimizer = torch.optim.ASGD(model.parameters())
-    targets = torch.tensor(arr, dtype=torch.float)
+    optimizer = torch.optim.ASGD(model.parameters(), lr=1e-3)
+    targets = torch.tensor(relationmap, dtype=torch.float)
 
-    for i in range(1000):
+
+    for i in range(500):
+        print("iteration is %d now." % i)
 
         optimizer.zero_grad()
         output = model(rows, cols)
@@ -44,5 +49,19 @@ if __name__ == '__main__':
         loss.backward()
         optimizer.step()
 
+    print("graph embedding completed.\n")
+
+    print("calculating Error...")
+
     result = model(rows, cols)
-    print(result)
+    sum = 0
+    for row, col in zip(rows, cols):
+        sum += np.square(result[row, col] - relationmap[row, col])
+    print("now Error is {}".format(sum))
+
+    curtime = datetime.datetime.now()
+    print("%d: %d: %d\n" % (curtime.hour, curtime.minute, curtime.second))
+
+
+if __name__ == '__main__':
+    train()
